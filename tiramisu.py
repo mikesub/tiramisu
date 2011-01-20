f371c93d2d9c62e57cf795cc757a8953d6a201cf
Mike Sub <allo.superman@gmail.com>
2011-01-20 07:01:22 +0300

    style up

--- tiramisu.py
+++ tiramisu.py
@@ -1,68 +1,76 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pyinotify, os, shutil, copy, sys, optparse
-from lxml import etree 
+STR = ''
+import pyinotify, os, shutil, sys, optparse
+from lxml import etree
 
 NS = {
     'xsl':'http://www.w3.org/1999/XSL/Transform',
     'xhtml':'http://www.w3.org/1999/xhtml'
 }
 
-HTML = ['a','abbr','acronym','address','area','b','base','bdo','big','blockquote','body','br','button','caption','cite','code','col','colgroup','dd','del','dfn','div','dl','dt','em','fieldset','form','h1','head','hr','html','i','img','input','ins','kbd','label','legend','li','link','map','meta','noscript','object','ol','optgroup','option','p','param','pre','q','samp','script','select','small','span','strong','style','sub','sup','table','tbody','td','textarea','tfoot','th','thead','title','tr','tt','ul','var']
-HTML_NS = ['//xhtml:'+i for i in HTML]
+HTML = ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'base', 'bdo', 'big', 'blockquote', 'body', 'br', 'button',
+        'caption', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'fieldset', 'form',
+        'h1', 'head', 'hr', 'html', 'i', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'meta',
+        'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'samp', 'script', 'select', 'small',
+        'span', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'title',
+        'tr', 'tt', 'ul', 'var']
+HTML_NS = ['//xhtml:' + i for i in HTML]
 XPATH = ' | '.join(HTML_NS)
 
-parser = optparse.OptionParser(usage = 'usage: %prog PATH_TO_XHH')
+parser = optparse.OptionParser(usage='usage: %prog PATH_TO_XHH')
 (options, args) = parser.parse_args()
 
 if not len(args):
     parser.print_help()
     sys.exit()
 
-SRC = os.path.normpath(os.path.join(os.getcwd(),args[0], 'xsl'))
+SRC = os.path.normpath(os.path.join(os.getcwd(), args[0], 'xsl'))
 DST = os.path.normpath(os.path.join(SRC, '../tiramisu'))
 
-filename = ''
+filename = STR
+
 
 if os.path.exists(DST):
     shutil.rmtree(DST)
-shutil.copytree(SRC,DST)
-    
+shutil.copytree(SRC, DST)
+
 def process_file(source, remove=False):
     global filename
     relative = os.path.relpath(source, SRC)
-    destination = os.path.join(DST,relative)
-    
+    destination = os.path.join(DST, relative)
+
     if remove:
         os.remove(destination)
-        print 'removed',destination
+        print 'removed', destination
         return
 
-    filename = os.path.normpath(os.path.join(DST,relative))
-    parse_file(source,destination)
+    filename = os.path.normpath(os.path.join(DST, relative))
+    parse_file(source, destination)
 
-def parse_file(source,destination):
+def parse_file(source, destination):
     global filename
-    
+
     try:
         xml = etree.parse(source)
-        elements = xml.xpath(XPATH,namespaces=NS)
+        elements = xml.xpath(XPATH, namespaces=NS)
         for element in elements:
-            element.set('line',str(element.sourceline))
-            element.set('file', str(str(filename).split('tiramisu/')[-1]) )
-        xml.write(destination,xml_declaration=True,encoding='utf-8')
-        print source,'->',filename
+            element.set('line', str(element.sourceline))
+            element.set('file', str(str(filename).split('tiramisu/')[-1]))
+        xml.write(destination, xml_declaration=True, encoding='utf-8')
+        print source, '->', filename
     except: pass
     filename = ''
 
 for root, dirs, files in os.walk(SRC):
     for file in files:
-       process_file('/'.join([root, file]))
-    
+        process_file('/'.join([root, file]))
+
 class ModifyHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
         process_file(event.pathname, remove=False)
+
     def process_IN_DELETE(self, event):
         process_file(event.pathname, remove=True)
 
@@ -70,9 +78,9 @@ wm = pyinotify.WatchManager()
 notifier = pyinotify.Notifier(wm, ModifyHandler())
 
 wm.add_watch(
-    SRC,
-    pyinotify.IN_MODIFY | pyinotify.IN_DELETE,
-    rec=True,
-    auto_add=True
-)
+        SRC,
+        pyinotify.IN_MODIFY | pyinotify.IN_DELETE,
+        rec=True,
+        auto_add=True
+        )
 notifier.loop()
